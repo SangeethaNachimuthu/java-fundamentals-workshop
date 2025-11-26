125c455518184bf683216eec6bef5e480d807013
Sangeetha Nachimuthu <sangeen.muthu@gmail.com>
2025-11-26 11:06:22 +0100

    Update the files

---
+++ README.md
@@ -0,0 +1,17 @@
+Pseudocode
+
+START
+    REPEAT
+        READ number1
+        READ number2
+        READ desiredOperation
+        SWITCH 
+            case a: perform addition and PRINT the result
+            case s: perform subtraction and PRINT the result
+            case m: perform multiplication and PRINT the result
+            case d: perform division and PRINT the result
+            case e: PRINT Exit Calculator App
+            default: PRINT choose correct operation
+        END SWITCH
+    END REPEAT
+END
--- src/main/java/CalculatorApp.java
+++ src/main/java/CalculatorApp.java
@@ -1,2 +1,74 @@
+import java.util.Scanner;
+
 public class CalculatorApp {
+
+    public static void main(String[] args) {
+
+        System.out.println("Calculator Application");
+        System.out.println("=======================");
+        System.out.println("The Calculator App performs Addition, Subtraction, " +
+                "Multiplication and Division operations.");
+        calculatorApp();
+    }
+
+    public static void calculatorApp() {
+        Scanner scan = new Scanner(System.in);
+        String desiredOperation;
+        int result;
+        do {
+            System.out.println("Enter number1: ");
+            int num1 = scan.nextInt();
+            System.out.println("Enter number2: ");
+            int num2 = scan.nextInt();
+            System.out.println("Choose any one of the operations to perform:");
+            System.out.println("Enter A for Addition, S for Subtraction, M for Multiplication" +
+                    " D for Division and E for Exit.");
+            desiredOperation = scan.next().toUpperCase();
+
+            switch (desiredOperation) {
+                case "A":
+                    result = addition(num1, num2);
+                    System.out.println("Addition: " + num1 + "+" + num2 + " = " + result);
+                    break;
+                case "S":
+                    result = subtraction(num1, num2);
+                    System.out.println("Subtraction: " + num1 + "-" + num2 + " = " + result);
+                    break;
+                case "M":
+                    result = multiplication(num1, num2);
+                    System.out.println("Multiplication: " + num1 + "*" + num2 + " = " + result);
+                    break;
+                case "D":
+                    if (num2 == 0) {
+                        System.out.println("Cannot divide by zero!");
+                    } else {
+                        result = division(num1, num2);
+                        System.out.println("Division: " + num1 + "/" + num2 + " = " + result);
+                    }
+                    break;
+                case "E":
+                    System.out.println("Thanks for using the Calculator App.");
+                    break;
+                default:
+                    System.out.println("Please enter the correct operation.");
+            }
+
+        } while (desiredOperation.equals("E"));
+    }
+
+    public static int addition(int num1, int num2) {
+        return (num1 + num2);
+    }
+
+    public static int subtraction(int num1, int num2) {
+        return (num1 - num2);
+    }
+
+    public static int multiplication(int num1, int num2) {
+        return (num1 * num2);
+    }
+
+    public static int division(int num1, int num2) {
+        return (num1 / num2);
+    }
 }
